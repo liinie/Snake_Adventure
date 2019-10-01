bd4cac09b58ad54dd9f31129f4ec363782e2d4d5
Lin Xu <lin.xujj@gmail.com>
2019-10-02 01:11:38 +0200

    edited game manager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public List<GameObject> snakeBodies;
+
+    public enum Direction {Left, Right, Up, Down};
+
+    public Direction movementDirection = Direction.Right;
+
+    public Direction lastMoveDirection = Direction.Right;
     void Start()
     {
         
@@ -13,6 +21,20 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            movementDirection = Direction.Right;
+        }else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            movementDirection = Direction.Left;
+        }else if (Input.GetKeyDown(KeyCode.UpArrow)){
+            movementDirection = Direction.Up;
+        }else if (Input.GetKeyDown(KeyCode.DownArrow)){
+            movementDirection = Direction.Down;
+        }
+
+        var newPosition = snakeBodies[0].transform.position;
+
         
+
+
     }
 }
